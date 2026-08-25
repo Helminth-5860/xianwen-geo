5c02aaefa7606284f4bf04ad2529fc33d01a0186
Helminth-5860 <5860428@qq.com>
2026-08-25 18:01:12 +0800

    feat: register website audit app

---
+++ backend/apps/website_audits/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WebsiteAuditsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.website_audits"
+    verbose_name = "官网深度检测"
