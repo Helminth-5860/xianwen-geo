8024c5fb9352525f48f50d185896fa348d3cf571
Helminth-5860 <5860428@qq.com>
2026-08-28 16:30:04 +0800

    feat: pause queued tasks when automatic publishing is disabled

--- backend/apps/publishing/views.py
+++ backend/apps/publishing/views.py
@@ -10,6 +10,7 @@ from apps.subjects.permissions import IsAvailableAuthenticatedUser
 
 from .authorization import begin_browser_authorization, sync_authorization_session
 from .models import PlatformAccount, PlatformAuthorizationSession, Publication, PublishingPreference
+from .pause_control import pause_subject_publications, resume_subject_publications
 from .review import approve_publication, is_waiting_review
 from .serializers import (
     AuthorizationStartSerializer,
@@ -81,11 +82,16 @@ class SubjectPublishingPreferenceView(APIView):
                 preference.custom_platform_keys = enabled_keys
                 preference.version += 1
                 preference.save(update_fields=("custom_platform_keys", "version", "updated_at"))
-        if preference.is_enabled and preference.mode in {
-            PublishingPreference.Mode.MANAGED,
-            PublishingPreference.Mode.REVIEW,
-        }:
-            adopt_ready_articles_task.delay(str(request.user.pk), str(subject_id))
+
+        if preference.is_enabled:
+            resume_subject_publications(user_id=request.user.pk, subject_id=subject_id)
+            if preference.mode in {
+                PublishingPreference.Mode.MANAGED,
+                PublishingPreference.Mode.REVIEW,
+            }:
+                adopt_ready_articles_task.delay(str(request.user.pk), str(subject_id))
+        else:
+            pause_subject_publications(user_id=request.user.pk, subject_id=subject_id)
         return Response({"preference": preference_payload(preference)})
 
 
